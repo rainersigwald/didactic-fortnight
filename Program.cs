b6ba7f619a487bae37676fbdc78f265978be10f3
Rainer Sigwald <raines@microsoft.com>
2019-09-27 15:59:21 -0500

    Minimal repro for https://github.com/dotnet/corefx/issues/40850

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace didactic_fortnight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Process.GetCurrentProcess().MainModule.FileName);
+        }
+    }
+}
